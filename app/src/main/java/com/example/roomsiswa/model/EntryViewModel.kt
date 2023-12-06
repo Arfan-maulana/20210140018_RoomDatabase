2e78ad9580e8b54a2d8e39b2e50aa4b3f5d2ae94
Arfan-maulana <114915872+Arfan-maulana@users.noreply.github.com>
2023-12-06 12:22:50 +0700

    membuat var yang berisi Status siswa saat ini

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -1,19 +1,33 @@
 package com.example.roomsiswa.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.roomsiswa.repository.ReporisitoriSiswa
 
 
-class EntryViewModel (private val repositoriSiswa: ReporisitoriSiswa): ViewModel(){
+class EntryViewModel (private val repositoriSiswa: ReporisitoriSiswa): ViewModel() {
 
+    /**
+     * Berisi Status Siswa Saat Ini
+     */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+
+    /**
+     * Mewakili Status Untuk Siswa
+     */
     data class UIStateSiswa(
-        val detailSiswa : DetailSiswa = DetailSiswa(),
-        val isEntryValid : Boolean = false
+        val detailSiswa: DetailSiswa = DetailSiswa(),
+        val isEntryValid: Boolean = false
     )
-    data class DetailSiswa (
-        val id : Int = 0,
-        val nama : String = "",
-        val alamat : String = "",
-        val telepon : String = "",
+
+    data class DetailSiswa(
+        val id: Int = 0,
+        val nama: String = "",
+        val alamat: String = "",
+        val telepon: String = "",
     )
 }
