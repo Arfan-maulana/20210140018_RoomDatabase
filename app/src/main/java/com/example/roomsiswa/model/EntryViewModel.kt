b4f93e32164e2a50fe196fd9fb3fba6888947004
Arfan-maulana <114915872+Arfan-maulana@users.noreply.github.com>
2023-12-06 12:16:46 +0700

    membuat data class UiStateSiswa di EntryViewModel

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -6,6 +6,10 @@ import com.example.roomsiswa.repository.ReporisitoriSiswa
 
 class EntryViewModel (private val repositoriSiswa: ReporisitoriSiswa): ViewModel(){
 
+    data class UIStateSiswa(
+        val detailSiswa : DetailSiswa = DetailSiswa(),
+        val isEntryValid : Boolean = false
+    )
     data class DetailSiswa (
         val id : Int = 0,
         val nama : String = "",
